5ce275bbd3515a4afa0b03710eb0c93ff0dccf1b
Paul McLagan <mclag011@umn.edu>
2023-06-09 10:27:05 -0500

    Adding script files to repo

---
+++ automationVariables.py
@@ -0,0 +1,25 @@
+from datetime import datetime, date
+
+API_URL = "https://canvas.umn.edu"
+DAYS_TO_SPRING_BREAK_START = 50
+
+# GUI Window Variables
+APP_TITLE = "The Jayback Machine: Canvas Migration and Update Automation Script"
+APP_TTK_THEME = "darkly"
+APP_HEIGHT = 1200
+APP_WIDTH = 900
+APP_TTK_WINDOW_SIZE = (APP_HEIGHT,APP_WIDTH)
+
+# Regex Varibles
+WEEK_SPAN_REGEX = '[A-Za-z]* (\d\d|\d)[ ]{0,}(-|–)[ ]{0,}[A-Za-z]* (\d\d|\d)'
+LIBRARY_EXT_REGEX = '(?<=courses\/)\d{6,}(?=\/external_tools\/12142)'
+
+# GUI DateEntry 
+FROM_DATEENTRY_START_DATE = datetime.today()
+TO_DATEENTRY_START_DATE = datetime.today()
+
+
+# Test Variables
+IS_TESTING = False
+FROM_CRS = None
+TO_CRS = None
